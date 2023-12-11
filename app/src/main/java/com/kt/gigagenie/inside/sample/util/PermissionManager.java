4ba82fc98214d1b542119bb94d18cd394886deb5
hangyuJeong <hangyu.jeong@kt.com>
2023-12-11 11:26:31 +0900

    Update Permission

--- app/src/main/java/com/kt/gigagenie/inside/sample/util/PermissionManager.java
+++ app/src/main/java/com/kt/gigagenie/inside/sample/util/PermissionManager.java
@@ -28,24 +28,33 @@ public class PermissionManager {
 
     private Activity activity;
     private static final int MY_PERMISSIONS_REQUEST = 1;
-    private String[] permissionList_TIRAMISU = {Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission
-            .ACCESS_NETWORK_STATE};
-    private String[] permissionList = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission
-            .ACCESS_NETWORK_STATE};
+
+    private String[] permissionList_TIRAMISU = {Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission.ACCESS_NETWORK_STATE};
+    private String[] permissionList_OREO = { Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission.ACCESS_NETWORK_STATE};
+    private String[] permissionList = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission.ACCESS_NETWORK_STATE};
 
     public PermissionManager(Activity activity) {
         this.activity = activity;
     }
 
     public boolean permissonCheck() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             for (String permission : permissionList_TIRAMISU) {
                 if (PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(activity, permission)) {
                     ActivityCompat.requestPermissions(activity, permissionList_TIRAMISU, MY_PERMISSIONS_REQUEST);
                     return false;
                 }
             }
-        } else {
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            for (String permission : permissionList_OREO) {
+                if (PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(activity, permission)) {
+                    ActivityCompat.requestPermissions(activity, permissionList_OREO, MY_PERMISSIONS_REQUEST);
+                    return false;
+                }
+            }
+        }
+        else {
             for (String permission : permissionList) {
                 if (PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(activity, permission)) {
                     ActivityCompat.requestPermissions(activity, permissionList, MY_PERMISSIONS_REQUEST);
